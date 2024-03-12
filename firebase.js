be95cdafbe51ae45a39f71601578e2b404cab387
Ahmed-eldaker <ahmeddaker01@gmail.com>
2024-03-12 23:24:08 +0200

    deploy firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATtGHgDkMbrywjPeTXhQmGMnVTXBO3vV0",
+  authDomain: "quotefy-7c180.firebaseapp.com",
+  projectId: "quotefy-7c180",
+  storageBucket: "quotefy-7c180.appspot.com",
+  messagingSenderId: "987193288719",
+  appId: "1:987193288719:web:8023bbe0e87f141a675b7a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
